906e486f1d148c109e83e5fa4747566fa2e0b414
tiziaco <tiziano.iaco@gmail.com>
2024-04-28 12:40:06 +0200

    add print methods

--- ex01/include/Contact.hpp
+++ ex01/include/Contact.hpp
@@ -6,20 +6,30 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 12:35:23 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/04/27 15:54:16 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/04/28 12:27:42 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#include <string>
+
 class Contact {
 	int id;
-	char *name, *last_name, *nickname, *phone_number, *dark_secret;
-public:
-	Contact(int, const char *, const char *, const char *, const char *, const char *);
-	~Contact();
-	void print_contact();
+	std::string name;
+	std::string last_name;
+	std::string nickname;
+	std::string phone_number;
+	std::string dark_secret;
+
+	public:
+		Contact(int, const std::string&, const std::string&, const std::string&,
+				const std::string&, const std::string&);
+		void print_formatted_raw_table();
+		void print_contact_details();
+	private:
+		void print_truncated(const std::string& str, size_t max_length);
 };
 
 #endif
