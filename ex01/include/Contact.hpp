39053126c84461b4153fbeadd15c6a0151cdcc09
tiziaco <tiziano.iaco@gmail.com>
2024-04-29 18:42:32 +0200

    finalize Contact class

--- ex01/include/Contact.hpp
+++ ex01/include/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 12:35:23 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/04/28 12:27:42 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/04/29 17:55:03 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,13 @@ class Contact {
 	public:
 		Contact(int, const std::string&, const std::string&, const std::string&,
 				const std::string&, const std::string&);
+		Contact();
+		void new_contact(int);
 		void print_formatted_raw_table();
 		void print_contact_details();
 	private:
-		void print_truncated(const std::string& str, size_t max_length);
+		std::string input_contact_details(std::string input_prompt);
+		std::string format_attr(const std::string& str);
 };
 
 #endif
--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -6,70 +6,86 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 12:35:23 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/04/28 12:27:35 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/04/29 18:32:27 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Contact.hpp"
 #include "../include/phone_book.h"
 
-/* Contact class Constructor */
+/* Contact class Constructors */
+Contact::Contact() {
+}
+
 Contact::Contact(int id, const std::string& name, const std::string& last_name,
-                 const std::string& nickname, const std::string& phone_number,
-                 const std::string& dark_secret) : id(id)
-{
-    this->name = name;
-    this->last_name = last_name;
-    this->nickname = nickname;
-    this->phone_number = phone_number;
-    this->dark_secret = dark_secret;
+				 const std::string& nickname, const std::string& phone_number,
+				 const std::string& dark_secret) : id(id) {
+	this->name = name;
+	this->last_name = last_name;
+	this->nickname = nickname;
+	this->phone_number = phone_number;
+	this->dark_secret = dark_secret;
 }
 
-void Contact::print_truncated(const std::string& str, size_t max_length)
-{
-        if (str.length() <= max_length)
-            std::cout << str;
-        else {
-            for (size_t i = 0; i < max_length - 1; ++i)
-                std::cout << str[i];
-            std::cout << ".";
-        }
+/* Private Class methods */
+std::string Contact::input_contact_details(std::string input_prompt) {
+	std::string input_str;
+	bool is_valid = false;
+
+	while (!is_valid) {
+		std::cout << input_prompt << std::flush;
+		getline(std::cin, input_str);
+		if (std::cin.good() && !input_str.empty())
+			is_valid = true;
+		else {
+			std::cin.clear();
+			std::cout << "Invalid input, please try again!" << std::endl;
+		}
+	}
+	return (input_str);
 }
 
-void Contact::print_contact_details()
-{
+std::string Contact::format_attr(const std::string& str) {
+		if (str.length() > 10)
+			return str.substr(0, 9) + ".";
+		else
+			return str;
+}
+
+/* Public Class methods */
+void Contact::new_contact(int index) {
+	std::cin.ignore();
+	this->name = input_contact_details("Enter first name: ");
+	this->last_name = input_contact_details("Enter last name: ");
+	this->nickname = input_contact_details("Enter nickname: ");
+	this->phone_number = input_contact_details("Enter phone number: ");
+	this->dark_secret = input_contact_details("Enter darkest secret: ");
+	this->id = index;
+	std::cout << std::endl;
+}
+
+void Contact::print_contact_details() {
+	if (this->name.empty() || this->last_name.empty() || this->nickname.empty()) {
+		std::cout << "Index not found" << std::endl;
+		return ;
+	}
 	std::cout << "ID: " << id << std::endl;
-	std::cout << "Name: " << name << "\n";
-	std::cout << "Last Name: " << last_name << "\n";
-	std::cout << "Nickname: " << nickname << "\n";
-	std::cout << "Phone Number: " << phone_number << "\n";
-	std::cout << "Dark Secret: " << dark_secret << "\n";
+	std::cout << "Name: " << name << std::endl;
+	std::cout << "Last Name: " << last_name << std::endl;
+	std::cout << "Nickname: " << nickname << std::endl;
 }
 
-void Contact::print_formatted_raw_table()
-{
-    // Print index
-    std::cout << std::right << std::setw(10) << id;
+void Contact::print_formatted_raw_table() {
+	if (this->name.empty() || this->last_name.empty() || this->nickname.empty())
+		return ;
+	// Print index
+	std::cout << std::right << std::setw(10) << this->id << std::flush;
+	// Print first name
+	std::cout << " | " << std::right << std::setw(10) << this->format_attr(this->name) << std::flush;
+	// Print last name
+	std::cout << " | " << std::right << std::setw(10) << this->format_attr(this->last_name) << std::flush;
+	// Print nickname
+	std::cout << " | " << std::right << std::setw(10) << this->format_attr(this->nickname) << std::flush;
 
-    // Print first name
-    std::cout << " | " << std::right << std::setw(10);
-    if (strlen(name.c_str()) <= 10)
-        std::cout << name;
-    else
-		print_truncated(name, 10);
-
-    // Print last name
-    std::cout << " | " << std::right << std::setw(10);
-    if (strlen(last_name.c_str()) <= 10)
-        std::cout << last_name;
-    else
-		print_truncated(last_name, 10);
-
-    // Print nickname
-    std::cout << " | " << std::right << std::setw(10);
-    if (strlen(nickname.c_str()) <= 10)
-        std::cout << nickname;
-    else
-		print_truncated(nickname, 10);
-    std::cout << std::endl;
+	std::cout << std::endl;
 }
