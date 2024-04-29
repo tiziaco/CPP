3e95d8bb1a529894fdef2ab9dfbc8e678bb8c739
tiziaco <tiziano.iaco@gmail.com>
2024-04-29 18:37:15 +0200

    refactor main function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 12:38:17 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/04/28 12:29:34 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/04/29 17:44:01 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,34 @@
 #include <string>
 #include <cstring>
 #include "include/Contact.hpp"
+#include "include/PhoneBook.hpp"
 
-int	main(int argc, char ** argv)
+void print_program_info()
 {
-	Contact cont(1, "Tiz", "Iaco", "superTiziaco", "377022", "idk");
-	cont.print_formatted_raw_table();
-	cont.print_contact_details();
+	std::cout << std::endl;
+	std::cout << "📖 Welcome to PhoneBook 📖" << std::endl;
+	std::cout << std::endl;
+	std::cout << "--------------USAGE---------------" << std::endl;
+	std::cout << "ADD\t: To add a contact." << std::endl;
+	std::cout << "SEARCH\t: To search for a contact." << std::endl;
+	std::cout << "EXIT\t: to quite the PhoneBook." << std::endl;
+	std::cout << "----------------------------------" << std::endl;
+	std::cout << std::endl;
+}
+
+int	main(int argc, char **argv)
+{
+	PhoneBook	book;
+	std::string	command = "";
+	
+	print_program_info();
+	while (command.compare("EXIT")) {
+		if (command.compare("ADD") == 0)
+			book.add_contact();
+		else if (command.compare("SEARCH") == 0)
+			book.search_contact();
+		std::cout << "> " << std::flush;
+		std::cin >> command;
+	}
 	return (0);
 }
