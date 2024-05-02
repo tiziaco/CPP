53990bb4967066ce46331d23a2ec7084ac289535
tiziaco <tiziano.iaco@gmail.com>
2024-05-02 17:36:05 +0200

    add ex02 CPP01

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/02 12:28:58 by tiacovel          #+#    #+#             */
+/*   Updated: 2024/05/02 12:29:17 by tiacovel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str = "HI THIS IS BRAIN";
+
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	// Print memory addresses
+	std::cout << "Memory address of the string variable: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	// Print values
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+	return 0;
+}
