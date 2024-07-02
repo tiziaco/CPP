7dc33b806a24fa7b22fd871f328f27f56420eb8f
tiziaco <tiziano.iaco@gmail.com>
2024-07-02 12:28:55 +0200

    remove comments

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tiacovel <tiacovel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:02:13 by tiacovel          #+#    #+#             */
-/*   Updated: 2024/07/02 12:25:09 by tiacovel         ###   ########.fr       */
+/*   Updated: 2024/07/02 12:28:16 by tiacovel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 #include "Point.hpp"
 
 int main() {
-	// Define your points
 	Point a(1.0f, 1.0f);
 	Point b(4.0f, 1.0f);
 	Point c(2.0f, 5.0f);
-	Point point(2.0f, 2.0f); // The point to check
+	Point point(2.0f, 2.0f);
 
-	// Call the bsp function and print the result
 	std::cout << "Triangle vertices:" << std::endl;
 	std::cout << "	A: x= " << a.getX() << ", y= " << a.getY() << std::endl;
 	std::cout << "	B: x= " << b.getX() << ", y= " << b.getY() << std::endl;
@@ -28,7 +26,7 @@ int main() {
 
 	std::cout << "Point:" << std::endl;
 	std::cout << "	P: x= " << point.getX() << ", y= " << point.getY() << std::endl;
-	//bsp(a, b, c, point);
+
 	if (bsp(a, b, c, point))
 		std::cout << "The point is inside the triangle." << std::endl;
 	else
